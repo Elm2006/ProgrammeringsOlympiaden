a337f5001ec16a6ba3af22c46ceeef62ef5db2c6
Elm2006 <isel06002@utb.vaxjo.se>
2023-11-24 12:32:16 +0100

    3

---
+++ Uppgift3OP/Uppgift3OP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Uppgift3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            string[] rad = new string[n];
+            char[,] rutnät = new char[n, m];
+            string ord = "";
+            for (int i = 0; i < n; i++)
+            {
+                rad[i] = Console.ReadLine();
+                for (int j = 0; j < rad[i].Length; j++)
+                {
+                    rutnät[i,j] = rad[i][j];
+                    if (rutnät[i, j] != '.')
+                    {
+                        ord += rutnät[i, j];
+                    }
+                }
+            }
+            Console.WriteLine(ord);
+        }
+    }
+}
