d8c4ea5ae7b7f33c6a6d6743ed69075b916e7b6f
Elm2006 <143172014+Elm2006@users.noreply.github.com>
2023-11-24 12:51:53 +0100

    alla

---
+++ Uppgift3OP/Uppgift1OP/Uppgift1OP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Uppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ord = Console.ReadLine();
+            int gånger = int.Parse(Console.ReadLine());
+            for (int i = 0; i < gånger; i++)
+            {
+                Console.Write(ord);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Uppgift3OP/Uppgift2OP/Uppgift2OP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Uppgift2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gånger = 0;
+            int n = int.Parse(Console.ReadLine());
+            for (int i = n-1; i > 0; i--)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j * (j + 1) * (j + 2))
+                    {
+                        gånger++;
+                    }
+                }
+            }
+            Console.WriteLine(gånger);
+        }
+    }
+}
